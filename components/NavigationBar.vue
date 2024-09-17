94f9408270a0dfa00a65f5f6a5fa10cc7cf7a8e0
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-09-17 20:48:25 +0900

    Applied Tailwind

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav>
-    <div>
-      <NuxtLink to="/">홈</NuxtLink>
+  <nav class="bg-gray-800 p-4 text-white">
+    <div class="flex justify-between items-center">
+      <NuxtLink to="/" class="bg-gray">홈</NuxtLink>
       <NuxtLink to="/search-room">검색</NuxtLink>
       <NuxtLink to="/create-room">개설</NuxtLink>
       <NuxtLink to="/">내 방</NuxtLink>
