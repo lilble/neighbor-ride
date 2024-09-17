d40756fdd372728535d842a6747fd57ba056a3d3
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-09-18 00:29:26 +0900

    Added Fontawesome

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,7 +1,10 @@
 <template>
-  <nav class="bg-gray-800 p-4 text-white">
+  <nav class="p-20 text-slate-400">
     <div class="flex justify-between items-center">
-      <NuxtLink to="/" class="bg-gray">홈</NuxtLink>
+      <div>
+        <NuxtLink to="/">홈</NuxtLink>
+        <font-awesome-icon icon="magnifying-glass" />
+      </div>
       <NuxtLink to="/search-room">검색</NuxtLink>
       <NuxtLink to="/create-room">개설</NuxtLink>
       <NuxtLink to="/">내 방</NuxtLink>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  css: ["~/assets/css/tailwind.css"],
+  css: [
+    "~/assets/css/tailwind.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  plugins: ["~/plugins/fontawesome.js"],
 });
