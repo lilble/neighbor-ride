a92c714ff6184241129e7be1bd7b74895f913c85
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-09-18 00:53:58 +0900

    working on fontawesome

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,14 +1,27 @@
 <template>
   <nav class="p-20 text-slate-400">
     <div class="flex justify-between items-center">
-      <div>
+      <div class="flex">
         <NuxtLink to="/">홈</NuxtLink>
-        <font-awesome-icon icon="magnifying-glass" />
+        <!-- <font-awesome-icon :icon="['fas', 'magnifying-glass']" /> -->
+        <font-awesome-icon icon="fa-solid fa-user-secret" />
       </div>
-      <NuxtLink to="/search-room">검색</NuxtLink>
-      <NuxtLink to="/create-room">개설</NuxtLink>
-      <NuxtLink to="/">내 방</NuxtLink>
-      <NuxtLink to="/">마이</NuxtLink>
+      <div>
+        <NuxtLink to="/search-room">검색</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/create-room">개설</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/">내 방</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/">마이</NuxtLink>
+      </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  plugins: ["~/plugins/fontawesome.js"],
+  // plugins: ["~/plugins/fontawesome.js"],
 });
